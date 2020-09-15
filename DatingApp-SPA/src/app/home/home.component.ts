4cf3d54e92fe8b47a06892fa910aff387f0ef2d7
unknown <vardan@hotmail.com>
2020-09-15 09:42:44 -0700

    End of section 4

---
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  values: any;
+  registerMode = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+
+  }
+
+  registerToggle() {
+    this.registerMode = true;
+  }
+
+  cancelRegisterMode(registerMode: boolean) {
+    this.registerMode = registerMode;
+  }
+
+}
